7d46b5701d1158a5094626ea07a371a7a16f7a20
Roxana Sultan <roxana.sultan@student.unitbv.ro>
2023-12-06 17:41:35 +0200

    Create window

--- Tancodrom/Tancodrom/Tancodrom.cpp
+++ Tancodrom/Tancodrom/Tancodrom.cpp
@@ -1,6 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <GL/glew.h>
+
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <glfw3.h>
+
 #include <iostream>
 
-int main()
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+int main(int argc, char** argv)
 {
+    std::string strFullExeFileName = argv[0];
+    std::string strExePath;
+    const size_t last_slash_idx = strFullExeFileName.rfind('\\');
+    if (std::string::npos != last_slash_idx)
+    {
+        strExePath = strFullExeFileName.substr(0, last_slash_idx);
+    }
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Tancodrom", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glewInit();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        double currentFrame = glfwGetTime();
+      
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
