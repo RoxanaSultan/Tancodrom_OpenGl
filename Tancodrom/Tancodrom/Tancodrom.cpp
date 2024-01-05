6d1531547b9e5823299cc35a340a2c1db658930b
Livia-unitbv <livia.toma@student.unitbv.ro>
2024-01-05 19:15:45 +0200

    Modified floor texture

--- Tancodrom/Tancodrom/Tancodrom.cpp
+++ Tancodrom/Tancodrom/Tancodrom.cpp
@@ -265,7 +265,7 @@ int main(int argc, char** argv)
 
     // load textures
     // -------------
-    unsigned int floorTexture = CreateTexture(strExePath + "\\skybox_bottom.jpg");
+    unsigned int floorTexture = CreateTexture(strExePath + "\\skybox_bottom2.jpg");
 
     // configure depth map FBO
     // -----------------------
